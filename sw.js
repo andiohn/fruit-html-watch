ac242814fb905589a1b7382060c07687ae8f03a0
Andjohn <30844240+andiohn@users.noreply.github.com>
2026-03-20 14:44:21 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'fruit-watch-v1';
+const ASSETS = [
+  'index.html',
+  'test.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
